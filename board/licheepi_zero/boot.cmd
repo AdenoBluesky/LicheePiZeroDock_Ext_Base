7085a79bd69e91d9040c89d9e603a7b94a65e3c5
adeno <adenobluesky@gmail.com>
2025-11-18 00:36:48 +0900

    dtb読み込み対応

--- board/licheepi_zero/boot.cmd
+++ board/licheepi_zero/boot.cmd
@@ -31,5 +31,5 @@ fi
 
 setenv bootargs console=ttyS0,115200 panic=5 console=tty0 rootwait root=/dev/mmcblk0p${rootfspart} earlyprintk rw
 load mmc 0:1 0x41000000 zImage
-load mmc 0:1 0x41800000 sun8i-v3s-licheepi-zero-dock.dtb
+load mmc 0:1 0x41800000 sun8i-v3s-licheepi-zero-dock-ex.dtb
 bootz 0x41000000 - 0x41800000
