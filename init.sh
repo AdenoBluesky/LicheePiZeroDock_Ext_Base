a13397f39a266aa1324ae0b3821e62c8bdda9821
adeno <adenobluesky@gmail.com>
2025-11-28 00:52:16 +0900

    手順

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# linuxのhashをコピーする
+cp ../board/sipeed/licheepi_zero/patches/linux/linux.hash patches/linux/
+
+# u-bootのhashをコピーする
+cp ../board/sipeed/licheepi_zero/patches/uboot/uboot.hash patches/uboot/
+
